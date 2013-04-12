71dbac51648a36a28ccbf499cbad080e3bdfd02f
Miga <ssockss914@hotmail.com>
2013-04-12 00:54:43 -0500

    first commit

---
+++ src/com/migapro/battery/BatteryWidget.java
@@ -0,0 +1,87 @@
+package com.migapro.battery;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.widget.RemoteViews;
+
+
+public class BatteryWidget extends AppWidgetProvider {
+	private static final String ACTION_BATTERY_UPDATE = "com.migapro.battery.action.UPDATE";
+	public static int batteryLevelLeft = 0;
+	
+	@Override
+	public void onEnabled(Context context) {
+		super.onEnabled(context);
+		
+		turnAlarmOnOff(context, true);
+		context.startService(new Intent(context, ScreenMonitorService.class));
+	}
+	
+	public static void turnAlarmOnOff(Context context, boolean turnOn) {
+		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+		Intent intent = new Intent(ACTION_BATTERY_UPDATE);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+		
+		if (turnOn) // Add extra 1 sec because sometimes ACTION_BATTERY_CHANGED is called after the first alarm
+			alarmManager.setRepeating(AlarmManager.RTC, System.currentTimeMillis() + 1000, 300 * 1000, pendingIntent);
+		else
+			alarmManager.cancel(pendingIntent);
+	}
+	
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		
+		// Sometimes when the phone is booting, onUpdate method gets called before onEnabled()
+		int tempLevelLeft = calculateBatteryLevel(context);
+		if (batteryLevelLeft != tempLevelLeft)
+			batteryLevelLeft = tempLevelLeft;
+		updateViews(context);
+	}
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		super.onReceive(context, intent);
+		
+		if (intent.getAction().equals(ACTION_BATTERY_UPDATE)) {
+			int tempLevelLeft = calculateBatteryLevel(context);
+			if (batteryLevelLeft != tempLevelLeft) {
+				batteryLevelLeft = tempLevelLeft;
+				updateViews(context);
+			}
+		}
+	}
+	
+	@Override
+	public void onDisabled(Context context) {
+		super.onDisabled(context);
+		
+		turnAlarmOnOff(context, false);
+		context.stopService(new Intent(context, ScreenMonitorService.class));
+	}
+	
+	private int calculateBatteryLevel(Context context) {
+		Intent batteryIntent = context.getApplicationContext().registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+		
+		int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+		int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, 100);
+		return level * 100 / scale;
+	}
+	
+	private void updateViews(Context context) {
+		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+		views.setTextViewText(R.id.batteryText, batteryLevelLeft + "%");
+		
+		ComponentName componentName = new ComponentName(context, BatteryWidget.class);
+		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+		appWidgetManager.updateAppWidget(componentName, views);
+	}
+
+}
---
+++ src/com/migapro/battery/ScreenMonitorService.java
@@ -0,0 +1,80 @@
+package com.migapro.battery;
+
+import android.app.KeyguardManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+
+public class ScreenMonitorService extends Service {
+	private static BroadcastReceiver screenOffReceiver;
+	private static BroadcastReceiver screenOnReceiver;
+	private static BroadcastReceiver userPresentReceiver;
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	}
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		registerScreenOffReceiver();
+		registerScreenOnReceiver();
+		registerUserPresentReceiver();
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		
+		unregisterReceiver(screenOffReceiver);
+		unregisterReceiver(screenOnReceiver);
+		unregisterReceiver(userPresentReceiver);
+	}
+	
+	private void registerScreenOffReceiver() {
+		screenOffReceiver = new BroadcastReceiver() {
+
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				BatteryWidget.turnAlarmOnOff(context, false);
+			}
+			
+		};
+		
+		registerReceiver(screenOffReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
+	}
+
+	private void registerScreenOnReceiver() {
+		screenOnReceiver = new BroadcastReceiver() {
+
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				KeyguardManager keyguardManager = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+				if (!keyguardManager.inKeyguardRestrictedInputMode())
+					BatteryWidget.turnAlarmOnOff(context, true);
+			}
+			
+		};
+
+		registerReceiver(screenOnReceiver, new IntentFilter(Intent.ACTION_SCREEN_ON));
+	}
+	
+	private void registerUserPresentReceiver() {
+		userPresentReceiver = new BroadcastReceiver() {
+
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				BatteryWidget.turnAlarmOnOff(context, true);
+			}
+			
+		};
+
+		registerReceiver(userPresentReceiver, new IntentFilter(Intent.ACTION_USER_PRESENT));
+	}
+	
+}
